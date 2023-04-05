49b45cc8c53543bfe7906f367a4ca701ef07f8f9
emanuel santoss <emanuelsantoss42@gmail.com>
2023-04-05 17:42:57 -0300

    Iniciando os estudo de importação

---
+++ primeiros-passos-com-javascript/aula 7 - importacao/main.js
@@ -0,0 +1,14 @@
+// ---- Codigos Equivante -----
+// const { nome } = pessoa;
+
+// const nome = pessoa.nome
+
+
+
+const { gets, print } = require("./funcoesAuxiliares");
+
+print(gets());
+
+const pessoa = {
+  nome: "vitor",
+};
